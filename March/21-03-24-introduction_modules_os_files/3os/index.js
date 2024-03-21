3aaf0245b37f4ed16c5a20ffef777e071e175d7c
salman3009 <salman444g@gmail.com>
2024-03-21 22:44:24 +0530

    code

--- March/21-03-24-introduction_modules_os_files/3os/index.js
+++ March/21-03-24-introduction_modules_os_files/3os/index.js
@@ -2,3 +2,9 @@ const global = require('os');
 
 let platform = global.platform();
 console.log(platform);
+
+let arch = global.arch();
+console.log(arch);
+
+let mem = global.totalmem();
+console.log(mem);
