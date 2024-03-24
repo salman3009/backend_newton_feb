70bfcd2c881096c9c07569ef350388aee1400dc9
salman3009 <salman444g@gmail.com>
2024-03-24 13:23:45 +0530

    code

--- March/24-03-24-http/1http/2_post_method.js
+++ March/24-03-24-http/1http/2_post_method.js
@@ -2,7 +2,21 @@ const http = require('http');
 
 const server = http.createServer((req,res)=>{
 
+    if(req.method == 'GET'){
+        res.writeHead(200,{'Content-Type':'text/plain'});
+
+        res.end("welcome to get method");
+    }
+
+
     if(req.method == 'POST'){
+
+        req.on('data',(input)=>{
+              const response = Buffer.from(input);
+              const result = response.toString();
+              console.log(result);
+
+        })
 
         res.writeHead(200,{'Content-Type':'text/plain'});
 
