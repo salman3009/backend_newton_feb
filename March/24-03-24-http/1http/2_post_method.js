c3b055180212619c07998aaca9b7b5ad53890325
salman3009 <salman444g@gmail.com>
2024-03-24 13:32:32 +0530

    code

--- March/24-03-24-http/1http/2_post_method.js
+++ March/24-03-24-http/1http/2_post_method.js
@@ -14,13 +14,23 @@ const server = http.createServer((req,res)=>{
         req.on('data',(input)=>{
               const response = Buffer.from(input);
               const result = response.toString();
-              console.log(result);
+              const object = JSON.parse(result);
+              console.log(object.age);
+
+              if(object.age>=18){
+                res.writeHead(200,{'Content-Type':'text/plain'});
+                res.end("You are eligible to vote");
+              }
+              else{
+                res.writeHead(400,{'Content-Type':'text/plain'});
+                res.end("You are not eligible to vote");
+              }
+
+            
 
         })
 
-        res.writeHead(200,{'Content-Type':'text/plain'});
-
-        res.end("welcome to post method");
+      
     }
 });
 
