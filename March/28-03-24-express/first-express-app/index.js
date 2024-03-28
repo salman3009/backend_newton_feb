ff1b337c43392424ff40ad383de5d0a52b4a6199
salman3009 <salman444g@gmail.com>
2024-03-28 21:47:24 +0530

    code

--- March/28-03-24-express/first-express-app/index.js
+++ March/28-03-24-express/first-express-app/index.js
@@ -1,4 +1,9 @@
-const app = require('./app');
+const express = require('express');
+const app = express();
+
+app.get('',(req,res)=>{
+    res.send("<h1>Hello world newton</h1>");
+})
 
 app.listen(8080,()=>{
     console.log("server is listening on port 8080");
