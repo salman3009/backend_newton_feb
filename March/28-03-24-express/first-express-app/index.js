ee4e1b1ab9ecf905295567d2facd0902bd309879
salman3009 <salman444g@gmail.com>
2024-03-28 21:55:00 +0530

    code

--- March/28-03-24-express/first-express-app/index.js
+++ March/28-03-24-express/first-express-app/index.js
@@ -5,6 +5,10 @@ app.get('',(req,res)=>{
     res.send("<h1>Hello world newton</h1>");
 })
 
+app.get('/person',(req,res)=>{
+    res.send("<p>Welcome to person routing</p>");
+})
+
 app.listen(8080,()=>{
     console.log("server is listening on port 8080");
 })
