fcf75d61c720fc624b90e0852c8d0a2123bdf3c8
salman3009 <salman444g@gmail.com>
2024-03-28 22:50:05 +0530

    code

--- March/28-03-24-express/second-express-app-nodemon/app.js
+++ March/28-03-24-express/second-express-app-nodemon/app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('',(req,res)=>{
-    res.send("welcome to express app");
+    res.send("to backend app");
+})
+
+app.get('/about',(req,res)=>{
+    res.send("welcome about page ");
 })
 
 module.exports = app;
