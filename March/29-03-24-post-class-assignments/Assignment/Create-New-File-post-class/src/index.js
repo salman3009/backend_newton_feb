786bfd076e61598cab80b070538653682df2ae87
salman3009 <salman444g@gmail.com>
2024-03-29 21:04:12 +0530

    Codd

---
+++ March/29-03-24-post-class-assignments/Assignment/Create-New-File-post-class/src/index.js
@@ -0,0 +1,16 @@
+const fs = require("fs/promises");
+const fileName = "myfile.txt";
+const fileContent = "Newton School";
+const writeFile = async (fileName, fileContent) => {
+    //Write your code here
+    //Don't change function name
+    try{
+       await fs.access(fileName);
+       console.log(`${fileName} already exists. Skipping write operation`);
+    }catch(err){
+       await fs.writeFile(fileName,fileContent);
+       console.log(`File ${fileName} created and data written successfully!`)
+    }
+  
+};
+module.exports =  writeFile ;
