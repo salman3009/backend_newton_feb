cfd74d0b5ed88313e0fba358c11c289d0d8618dd
salman3009 <salman444g@gmail.com>
2024-03-31 14:53:06 +0530

    Code

--- March/31-03-24-environment-params-middleware/Assignment/Defining-a-route-with-a-parameter-for-user/src/index.js
+++
@@ -1,5 +0,0 @@
-const app = require('./app');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-
-app.listen(3000, () => console.log('Server running......'));
--- March/31-03-24-environment-params/Assignment/Defining-a-route-with-a-parameter-for-user/src/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/user/:userId', (req, res) => {
-  res.send({userId:req.params.userId});
-  //Write a code here for endpoint /user/:userId and also print parameter in json form
-});
-
-module.exports = app;
