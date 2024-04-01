3f0d18a48a4e94f701c591a3f9cf24473f6d2d1c
salman3009 <salman444g@gmail.com>
2024-04-01 22:33:06 +0530

    code

--- April/01-04-24/Ecommerce-GET-Products/app.js
+++ April/01-04-24/Ecommerce-GET-Products/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const cors = require('cors');
 
 const products = JSON.parse(fs.readFileSync('./data/products.json'));
 console.log(products);
+
+app.use(cors());
 
 app.get('/api/v1/products',(req,res)=>{
     res.status(200).json({
