e49d3ce5756f803ab27e5f958f22893eb3cd82a6
salman3009 <salman444g@gmail.com>
2024-04-02 22:28:53 +0530

    Code

--- April/02-04-24/ecommerce-products-post-method/app.js
+++ April/02-04-24/ecommerce-products-post-method/app.js
@@ -8,64 +8,72 @@ console.log(products);
 
 app.use(express.json());
 
-app.get('/api/v1/products',(req,res)=>{
+app.get('/api/v1/products', (req, res) => {
+  res.status(200).json({
+    status: "Success",
+    message: "successfully fetched",
+    data: {
+      products
+    }
+  })
+})
+
+app.get("/api/v1/products/:id", (req, res) => {
+
+  let id = Number(req.params.id);
+  console.log(id);
+
+  const product = products.find((obj) => {
+    return obj.id == id;
+  })
+
+  if (!product) {
+    res.status(404).json({
+      status: "failed",
+      message: "Product not found!"
+    })
+  }
+  else {
     res.status(200).json({
-        status:"Success",
-        message:"successfully fetched",
-        data:{
-          products 
-        }
+      status: "success",
+      message: "Product fetched successfully",
+      data: {
+        product
+      }
     })
+  }
 })
 
-app.get("/api/v1/products/:id",(req,res)=>{
+app.post("/api/v1/products", (req, res) => {
 
-    let id = Number(req.params.id);
-    console.log(id);
-   
-    const product = products.find((obj)=>{
-      return obj.id == id;
+  console.log(products);
+  let id;
+  if (products.length > 0) {
+     id = products[products.length - 1].id + 1;
+  }
+  else {
+     id = 1;
+  }
+
+  console.log(id);
+  const { name, price, stocks } = req.body;
+
+  const newProduct = { id, name, price, stocks };
+  products.push(newProduct);
+
+  fs.writeFile('./data/products.json', JSON.stringify(products), () => {
+    res.status(201).json({
+      status: "success",
+      message: "Product registered successfully",
+      data: {
+        productDetails: newProduct
+      }
     })
-
-    if(!product){
-      res.status(404).json({
-        status:"failed",
-        message:"Product not found!"
-      })
-    }
-    else{
-      res.status(200).json({
-        status:"success",
-        message:"Product fetched successfully",
-        data:{
-          product
-        }
-      })
-    }
-})
-
-app.post("/api/v1/products",(req,res)=>{
-      
-      const id = products[products.length-1].id+1;
-      console.log(id);
-      const {name,price,stocks} = req.body;
-
-      const newProduct={id,name,price,stocks};
-      products.push(newProduct);
-      
-      fs.writeFile('./data/products.json',JSON.stringify(products),()=>{
-        res.status(201).json({
-          status:"success",
-          message:"Product registered successfully",
-          data:{
-            productDetails:newProduct
-          }
-         })
-      })
+  })
 
 
 
 
-    
+
 })
 module.exports = app;
