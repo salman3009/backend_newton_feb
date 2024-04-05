e049262530921050fbeb49f2a861a412b3b64742
salman3009 <salman444g@gmail.com>
2024-04-05 21:20:27 +0530

    code

---
+++ April/05-04-24-post-class-assignment/2_product_name_price/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+const products = JSON.parse(fs.readFileSync('./data/products.json'));
+
+app.use(express.json());
+
+app.get("/api/v1/products/:name/:price",(req,res)=>{
+     const name = req.params.name;
+     const price = Number(req.params.price);
+
+     const product = products.find((obj)=>{
+       return obj.name == name && obj.price == price;
+     });
+
+     if(!product){
+        return res.status(404).json({
+            status:"failed",
+            message:"Product not found!"
+        })
+     }
+     else{
+        return res.status(200).json({
+            status:"success",
+            message:"Product fetched successfully",
+            data:{
+                product
+            }
+        })
+     }
+
+
+})
+
+module.exports = app;
