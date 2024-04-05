779da2d99943d1fc362401a849081ac44c1cb6b8
salman3009 <salman444g@gmail.com>
2024-04-05 22:21:13 +0530

    code

--- April/05-04-24-post-class-assignment/assignment/Get-name-using-id-HTTP-GET-Request/src/app.js
+++ April/05-04-24-post-class-assignment/assignment/Get-name-using-id-HTTP-GET-Request/src/app.js
@@ -32,7 +32,7 @@ app.get("/api/v1/names/:id",(req,res)=>{
            status:"success",
            message:"Product name fetched successfully",
            data:{
-               names:product
+               name:product
            }
        })
     }
