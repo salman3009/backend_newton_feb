e491267ccc228fb61f4a199ec892795824ee319d
salman3009 <salman444g@gmail.com>
2024-04-05 22:42:38 +0530

    code

---
+++ April/05-04-24-post-class-assignment/assignment/Buying-an-E-Commerce-Product/src/app.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+
+// Importing products from products.json file
+const products = JSON.parse(
+    fs.readFileSync(`${__dirname}/data/product.json`)
+);
+
+
+// Middlewares
+app.use(express.json());
+
+// Write PATCH endpoint to buy a product for the client here
+// Endpoint /api/v1/products/:id
+
+app.patch("/api/v1/products/:id",(req,res)=>{
+    
+    const id = Number(req.params.id);
+
+    const product = products.find((obj)=>{
+        return obj.id == id;
+    })
+
+    if(!product){
+     return res.status(404).json({
+         status:"failed",
+         message:"Product not found!"
+     })
+    }
+    else{
+     product.quantity-=1;
+     if(product.quantity>0){
+         return res.status(200).json({
+              status:"success",
+              message:`Thank you for purchasing ${product.name}`,
+              data:{
+                 product
+              }
+         })
+     }
+     else{
+         return res.status(404).json({
+             status:"success",
+             message:`${product.name}, Out of stock!`
+         }) 
+     }
+
+    }
+})
+
+
+
+module.exports = app;
---
+++ April/05-04-24-post-class-assignment/assignment/Buying-an-E-Commerce-Product/src/index.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+const dotenv = require('dotenv');
+
+
+
+dotenv.config();
+
+
+app.listen(3000, () => console.log('Server running......'));
+
