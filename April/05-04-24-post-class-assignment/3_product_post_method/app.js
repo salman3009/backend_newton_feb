eb88a185c9513590b18e7d22de2a248b04ab8f9d
salman3009 <salman444g@gmail.com>
2024-04-05 21:44:12 +0530

    code

---
+++ April/05-04-24-post-class-assignment/3_product_post_method/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+const products = JSON.parse(fs.readFileSync('./data/products.json'));
+
+app.use(express.json());
+
+app.post("/api/v1/products",(req,res)=>{
+    
+       const id = products[products.length-1].id+1;
+       const{name,price,quantity}=req.body;
+
+       const newProduct={id,name,price,quantity};
+
+       products.push(newProduct);
+
+       fs.writeFile('./data/products.json',JSON.stringify(products),()=>{
+        return res.status(201).json({
+            status:"success",
+            message:"Product added successfully",
+            data:{
+                newProduct
+            }
+        })
+       })
+ 
+    
+      
+     
+
+
+})
+
+module.exports = app;
