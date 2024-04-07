58f92feb3ed43a2db5923c57da51c85408712873
salman3009 <salman444g@gmail.com>
2024-04-07 14:54:21 +0530

    code

--- April/07-4-24-weather-api/assignment/Weather-API-Level-3-Get-ZipCode/src/routes/weatherRoutes.js
+++ April/07-4-24-weather-api/assignment/Weather-API-Level-3-Get-ZipCode/src/routes/weatherRoutes.js
@@ -36,7 +36,14 @@ const weatherController = require('../controllers/weatherController');
 
 // Level 3: Get City Weather Data by ZipCode
 router.get('/city/zipcode/:code', async (req, res) => {
-   // TODO: Implement this function
+  try {
+    const zipCode = req.params.code;
+    const weatherData = await weatherController.getWeatherDataByZipCode(zipCode);
+    res.status(200).json({ status: 'success', message: 'Weather data retrieved', data: weatherData });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ status: 'error', message: 'Failed to retrieve weather data', error: error.message });
+  }
 });
 
 module.exports = router;
