8312299958c72bc829cb76e8b0bb1fd5ca72aa03
salman3009 <salman444g@gmail.com>
2024-04-12 21:23:19 +0530

    code

--- April/12-04-24-post-class/mvc/src/app.js
+++ April/12-04-24-post-class/mvc/src/app.js
@@ -3,6 +3,8 @@ const app = express();
 const tourRoutes = require('./routes/tourRoutes');
 
 app.use(express.json());
+app.use('',express.static(`${__dirname}/views`));
 app.use('/api/v1/tours',tourRoutes);
+
 
 module.exports = app;
