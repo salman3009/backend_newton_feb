da7449428cf69b7a760f490e8117d08134e7bc1b
salman3009 <salman444g@gmail.com>
2024-04-14 13:27:08 +0530

    Code

--- April/14-04-24-crud_mongoose/crud/src/index.js
+++ April/14-04-24-crud_mongoose/crud/src/index.js
@@ -1,7 +1,14 @@
 const app = require('./app');
 const dotenv =require('dotenv');
-
+const mongoose = require('mongoose');
 dotenv.config();
+
+mongoose.connect(process.env.URI).then(()=>{
+    console.log("Database is connected")
+}).catch((err)=>{
+    console.log(err);
+})
+
 
 app.listen(process.env.PORT,()=>{
     console.log(`server is running on ${process.env.PORT}`)
