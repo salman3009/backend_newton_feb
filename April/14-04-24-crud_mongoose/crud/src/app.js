ac0101cfd27b88568ba035ec69cf64f39c628e83
salman3009 <salman444g@gmail.com>
2024-04-14 13:21:09 +0530

    Code

---
+++ April/14-04-24-crud_mongoose/crud/src/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const userRoutes = require('./routes/userRoutes');
+const app = express();
+
+app.use(express.json());
+app.use('/api/v1/users',userRoutes);
+
+module.exports = app;
