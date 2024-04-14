a10c407ca9b031fbf857b87879b6e38f65879cde
salman3009 <salman444g@gmail.com>
2024-04-14 14:34:44 +0530

    code

--- April/14-04-24-crud_mongoose/assignment/CURD_Operations-User/model/userModel.js
+++ April/14-04-24-crud_mongoose/assignment/CURD_Operations-User/model/userModel.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
-// Define the user schema
 const userSchema = new mongoose.Schema({
-  // Implement the user schema fields:
-  // name as String and
-  // email as a String,
+  name: String,
+  email: String,
 });
 
-// Create and export the User model
+module.exports = mongoose.model('User', userSchema);
