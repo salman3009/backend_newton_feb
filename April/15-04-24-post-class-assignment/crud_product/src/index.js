e69ddd8043a0db59366f6f87500039315d27cbbc
salman3009 <salman444g@gmail.com>
2024-04-15 21:16:53 +0530

    code

---
+++ April/15-04-24-post-class-assignment/crud_product/src/index.js
@@ -0,0 +1,15 @@
+const app = require('./app');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+mongoose.connect(process.env.URI).then(()=>{
+    console.log("database is connected");
+}).catch((err)=>{
+    console.log(err);
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server is running on ${process.env.PORT}`);
+})
