0ffc9d168de6a506c6faf28060394f7da3c6e743
salman3009 <salman444g@gmail.com>
2024-04-16 22:14:34 +0530

    code

--- April/16-04-24-authentication_authorization/authentication_crud_product/src/middlewares/isLogged.js
+++ April/16-04-24-authentication_authorization/authentication_crud_product/src/middlewares/isLogged.js
@@ -2,6 +2,16 @@
 
 function isLogged(req,res,next){
     console.log("middleware");
-    next();
+    console.log(req.headers.authorization);
+    if(req.headers.authorization == "xyz"){
+        next();
+    }
+    else{
+        res.status(401).json({
+          status:"failed",
+          message:"authentication failed,please provide valid token"
+        })
+    }
+   
 }
 module.exports= isLogged;
